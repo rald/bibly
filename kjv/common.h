f1428e1cdaf8cbdac433a8ac2fb724f742606708
rald <thecoconutnutisagiantnut@gmail.com>
2025-02-07 20:51:45 +0800

    OK

--- kjv/common.h
+++ kjv/common.h
@@ -1,8 +1,9 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#define CSV_FILE "/data/data/com.termux/files/home/bibly/kjv/kjv.csv"
-#define INFO_FILE "/data/data/com.termux/files/home/bibly/kjv/kjv.inf"
+#define CSV_FILE "/data/data/com.termux/files/home/x/bibly/kjv/kjv.csv"
+
+#define INFO_FILE "/data/data/com.termux/files/home/x/bibly/kjv/kjv.inf"
 
 #define STRING_MAX 65536
 
